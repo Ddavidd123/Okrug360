2b762b9b5ce78e3d92fe79dac9b940f4269577ac
Ddavidd123 <stosicdavid6@gmail.com>
2026-07-16 16:55:34 +0200

    setup: entity framework core

--- Okrug360.Content.Api/Data/ContentDbContext.cs
+++ Okrug360.Content.Api/Data/ContentDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace Okrug360.Content.Api.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Okrug360.Content.Api.Enttities;
+using System.Collections.Generic;
+
+namespace Okrug360.Content.Api.Data;
+
+public class ContentDbContext : DbContext
 {
-    public class ContentDbContext
+    public ContentDbContext(DbContextOptions<ContentDbContext> options)
+    : base(options)
     {
     }
+
+    public DbSet<NewsArticle> NewsArticles => Set<NewsArticle>();
 }
