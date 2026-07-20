908d51c8b2705283f4a6ee834d72aaff06539ba8
Ddavidd123 <stosicdavid6@gmail.com>
2026-07-20 19:23:39 +0200

    Dodavanje servisa Places, entitet i enum

---
+++ Okrug360.Places.Api/Entities/Place.cs
@@ -0,0 +1,31 @@
+﻿using Okrug360.Places.Api.Enums;
+
+namespace Okrug360.Places.Api.Entities
+{
+    public class Place
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public PlaceCategory Category { get; set; }
+
+        public string Address { get; set; } = string.Empty;
+
+        public string City { get; set; } = "Vranje";
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get;set; }
+
+        public DateTime? PublishedAt {  get; set; }
+
+        public bool IsPublished {  get; set; }
+    }
+}
---
+++ Okrug360.Places.Api/Enums/PlaceCategory.cs
@@ -0,0 +1,14 @@
+﻿namespace Okrug360.Places.Api.Enums
+{
+    public enum PlaceCategory
+    {
+        Restaurant = 0,
+        Cafe = 1,
+        Monestery = 2,
+        Museum = 3,
+        Park = 4,
+        CulturalSite = 5,
+        Government = 6,
+        Other = 7
+    }
+}
